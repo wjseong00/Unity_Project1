9c934785063700846f606d90cdfbd1ac2421277f
wonjin0217 <yssung021@gmail.com>
2020-06-03 11:38:49 +0900

    에너미클래스 및 애너미 매니져 생성
    - 에너미 프리팹
    - 충돌처리 (에너미와 플레이어, 플레이어 총알)
    - 에너미 매니져를 통한 자동 생성
    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    - Visual Studio 버전 : Visual Community 2017

---
+++ New Unity Project/Assets/_SWJ/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //위에서 아래로 떨어지기만 한다 (똥피하기 느낌)
+    //충돌처리 (에너미랑 플레이어, 에너미랑 플레이어 총알)
+
+
+    public float speed = 10.0f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //아래로 이동해라
+        transform.Translate(speed * Time.deltaTime * Vector3.down);
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //자기자신도 없애고
+        //충돌된 오브젝트도 없앤다
+        Destroy(gameObject);
+        Destroy(collision.gameObject);
+    }}
---
+++ New Unity Project/Assets/_SWJ/Scripts/EnemyManager.cs
@@ -0,0 +1,53 @@
+﻿//using System;       //이놈이 있으면 Random 함수를 사용 불가
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    //에너미매니저 역할?
+    //에너미들을 공장에서 찍어낸다
+    //에너미 스폰타임
+    //에너미 스폰위치
+
+    public GameObject enemyFactory;     //에너미 공장(에너미프리팹)
+    //public GameObject spawnPoint;       //스폰위치
+    public GameObject[] spawnPoints;       //스폰위치 여러개
+    float spawnTime = 1.0f;                    //스폰타임 (몇초에 한번씩 찍어낼거냐?)
+    float curTime=0.0f;                      //누적타임
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //에너미 생성
+        SpawnPoint();
+    }
+
+    private void SpawnPoint()
+    {
+        //몇초에 한번씩 이벤트 발동
+        //시간 누적타임으로 계산한다
+        //게임에서 정말 자주 사용함
+
+        curTime += Time.deltaTime;
+        if(curTime > spawnTime)
+        {
+            //누적된 현재시간을 0.0초로 초기화(반드시 해줘야 한다)
+            curTime = 0.0f;
+
+            //스폰타임을 랜덤으로
+            spawnTime = Random.Range(0.5f, 2.0f);
+
+
+            //에너미 생성
+            GameObject enemy = Instantiate(enemyFactory);
+            //enemy.transform.position = spawnPoint.transform.position;
+            int index = Random.Range(0, spawnPoints.Length);
+            //enemy.transform.position = transform.GetChild(index).transform.position;
+            enemy.transform.position = spawnPoints[index].transform.position;
+            
+        }
+    }
+}
