4ac9dbeb06ad4281313df633b05d24c99e801b64
wonjin0217 <yssung021@gmail.com>
2020-06-05 14:12:18 +0900

    Line Renderer 이용한 레이저 생성
    - 레이저가 일정시간 지난 후 사라지도록 구현
    - 레이저가 적과 충돌 시 충돌지점까지 길이가 만들어지며 삭제되도록 구현
    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    - Visual Studio 버전 : Visual Community 2017

--- New Unity Project/Assets/_SWJ/Scripts/PlayerFire.cs
+++ New Unity Project/Assets/_SWJ/Scripts/PlayerFire.cs
@@ -10,11 +10,25 @@ public class PlayerFire : MonoBehaviour
     public GameObject subPet2;
     public GameObject firePoint;
 
+    //레이저를 발사하기 위해서는 라인렌더러가 필요하다
+    //선은 최소 2개의 점이 필요하다(시작점, 끝점)
+    LineRenderer lr;     //라인렌더러 컴포넌트
+    float curTime = 0.0f;
+    float FireTime = 1.0f;
+    float timer = 0.0f;
+    float rayTime = 0.5f;
+    //private RaycastHit hit;
+    //Ray ray;
+
     int count = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //라인렌더러 컴포넌트 추가
+        lr = GetComponent<LineRenderer>();
+        //중요!!!
+        //게임오브젝트는 활성화 비활성화 => SetActive() 함수 사용
+        //컴포넌트는 enabled 속성 사용
     }
 
     // Update is called once per frame
@@ -52,9 +66,103 @@ public class PlayerFire : MonoBehaviour
             subPet1.SetActive(false);
             subPet2.SetActive(false);
         }
-        Fire();
+        //Fire();
+        FireRay();
+        //레이저를 보여준다
+        //일정 시간이 지나면 레이저 보여주는기능비활성화
+
+        if (lr.enabled) showLayer();
+        //if(lr.enabled)
+        //{
+        //    curTime += Time.deltaTime;
+        //    if(curTime >FireTime)
+        //    {
+        //        lr.enabled = false;
+        //    }
+        //}
+        
+        
+    }
+    void showLayer()
+    {
+        if (lr.enabled)
+        {
+            timer += Time.deltaTime;
+            if (timer >rayTime)
+            {
+                lr.enabled = false;
+            }
+        }
+    }
+    //레이저 발사
+    public void FireRay()
+    {
+        //마우스 왼쪽 버튼 or 왼쪽 컨트롤 키
+        if (Input.GetButtonDown("Fire1"))
+        {
+            curTime = 0;
+            timer = 0;
+            //라인렌더러 컴포넌트 활성화
+            lr.enabled = true;
+            //라인 시작점, 끝점
+            //Vector3 pos = transform.position;
+            //lr.SetPosition(0, transform.position);
+            //ray = new Ray(transform.position, Vector3.up * 10);
+            //Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+            //Debug.Log("포인트 : " + hit.point);
+            //if (Physics.Raycast(ray, out hit, 10))
+            //{
+            //    if(hit.collider.name =="Boss")
+            //    {
+            //        Debug.Log("IN");
+            //        lr.SetPosition(1, hit.point);
+            //    }
+            //    else
+            //    {
+            //        lr.SetPosition(1, transform.position + Vector3.up * 10);
+            //    }
+            //    
+            //}
+            //else
+            //{
+            //    lr.SetPosition(1, transform.position + Vector3.up * 10);
+            //}
+            lr.SetPosition(0, transform.position);
+            //라인의 끝점은 충돌된 지점으로 변경한다.
+            Ray ray = new Ray(transform.position, Vector3.up);
+            RaycastHit hitInfo; //Ray의 충돌된 오브젝트의 정보를 담는다.
+            if(Physics.Raycast(ray,out hitInfo))
+            {
+                //레이저의 끝점 지정
+                lr.SetPosition(1, hitInfo.point);
+                //충돌된 오브젝트 모두 지우기
+                Destroy(hitInfo.collider.gameObject);
+
+                //디스토리존의 탑과는 충돌처리 되지 않도록 한다.
+                if(hitInfo.collider.name != "Top")
+                {
+                    Destroy(hitInfo.collider.gameObject);
+                }
+
+                //충돌된 에너미 오브젝트 삭제
+                //Contains("Enemy") => Enemy(clone) 이런것도포함함
+                //if (hitInfo.collider.name.Contains("Enemy"))
+                //{
+                //    Destroy(hitInfo.collider.gameObject);
+                //}
+            }
+            else
+            {
+                //충돌된 오브젝트가 없으니 끝점을 정해준다.
+                lr.SetPosition(1, transform.position + Vector3.up * 10);
+            }
+        }
     }
 
+    
+
+
+    //총알 발사
     private void Fire()
     {
         //마우스 왼쪽 버튼 or 왼쪽 컨트롤 키
